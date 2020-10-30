a3745437c9599259d1387f35fd4b6581100e45f6
FLOWZ <ketuimevbore@yahoo.com>
2020-10-30 00:31:24 -0700

    Logout feature implemented as a top right corner single clickable menuitem in MainActivity

--- app/src/main/java/edu/android/thinkr/MainActivity.kt
+++ app/src/main/java/edu/android/thinkr/MainActivity.kt
@@ -1,11 +1,38 @@
 package edu.android.thinkr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_module)
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_logout, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+
+            R.id.logout->{
+                auth.signOut()
+                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
